d49a3b08386f4eb209b0ed26d8910fa2939de6d9
ericchi00 <ericchi00@gmail.com>
2022-05-15 16:10:09 -0700

    added in home route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import Home from './components/Home';
+import Register from './components/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-	return <div className="memes">test post</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Header></Header>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 };
 
 export default App;
