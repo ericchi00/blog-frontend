ab48c3223058d258b97f9ed1404ca28c4a78915f
ericchi00 <ericchi00@gmail.com>
2022-05-15 16:10:25 -0700

    added in Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,2 +1,26 @@
-import React from "react";
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
+const Header = () => {
+	return (
+		<Navbar bg="dark" variant="dark">
+			<Container fluid className="">
+				<Navbar.Brand href="/">Generic Blog</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="/">Home</Nav.Link>
+				</Nav>
+				<Button href="/register" variant="light" className="me-2">
+					Register
+				</Button>
+				<Button href="/login" variant="light">
+					Login
+				</Button>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default Header;
