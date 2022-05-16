ce7a9c5f9b8a515fb4f3063ba405c75e8ade2da0
ericchi00 <ericchi00@gmail.com>
2022-05-16 14:14:41 -0700

    moved tinymce to new page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Login from './components/Login';
-import Home from './components/Home';
-import Register from './components/Register';
+import Login from './components/Routes/Login';
+import Home from './components/Homepage/Home';
+import Register from './components/Routes/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MessageForm from './components/Routes/MessageForm';
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 				<Header></Header>
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/createpost" element={<MessageForm />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
 				</Routes>
