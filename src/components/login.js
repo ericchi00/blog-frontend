1fc21d01076566a359ee565c95cce8f4662028be
ericchi00 <ericchi00@gmail.com>
2022-05-15 19:35:45 -0700

    added in styling and input fields

--- src/components/login.js
+++ src/components/login.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
+import React, { useEffect, useState } from 'react';
+import { Form as BootstrapForm } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import { Formik, Form } from 'formik';
+import InputField from './InputField';
 
 const Login = () => {
+	useEffect(() => {
+		document.title = 'Login Form';
+	});
+
+	const login = () => {};
+
 	return (
 		<>
-			<Container fluid className="mt-3 w-50">
-				<Form>
-					<Form.Group className="mb-3" controlId="formBasicUsername">
-						<Form.Label>Username</Form.Label>
-						<Form.Control type="text" />
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" />
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Login
-					</Button>
-				</Form>
+			<style type="text/css">
+				{`.container-lg {
+                        max-width: 600px;
+                    }
+                    .btn {
+                        float: right;
+                    }`}
+			</style>
+			<Container fluid="lg" className="mt-5">
+				<Formik
+					initialValues={{
+						username: '',
+						password: '',
+					}}
+					onSubmit={(values, { setSubmitting }) => {
+						setTimeout(() => {
+							alert(JSON.stringify(values, null, 2));
+							setSubmitting(false);
+						}, 400);
+					}}
+				>
+					<Form className="border p-5 rounded border-3">
+						<div className="card-body p-2 text-center">
+							<h3 className="mb-1">Login Form</h3>
+						</div>
+						<BootstrapForm.Group className="mb-3 w-auto">
+							<InputField
+								label="Username"
+								name="username"
+								type="text"
+								className="form-control"
+							/>
+						</BootstrapForm.Group>
+						<BootstrapForm.Group className="mb-3">
+							<InputField
+								label="Password"
+								name="password"
+								type="password"
+								className="form-control"
+							/>
+						</BootstrapForm.Group>
+						<Button variant="primary" type="submit">
+							Login
+						</Button>
+					</Form>
+				</Formik>
 			</Container>
 		</>
 	);
