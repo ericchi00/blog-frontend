f113e4f4871185a50084c935180b25794c887dea
ericchi00 <ericchi00@gmail.com>
2022-05-17 19:57:52 -0700

    moved routes out to new file

---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RequireAuth } from 'react-auth-kit';
+import Header from '../Header';
+import Login from './Login';
+import Home from '../Homepage/Home';
+import Register from './Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MessageForm from './MessageForm';
+import Post from '../../Post';
+
+const RoutesComponent = () => {
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route
+					path="/createpost"
+					element={
+						<RequireAuth loginPath={'/login'}>
+							<MessageForm />
+						</RequireAuth>
+					}
+				/>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/posts/:id" element={<Post />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+export default RoutesComponent;
