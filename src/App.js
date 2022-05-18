0011104c209626afe859ae5218be2edf06e735fe
ericchi00 <ericchi00@gmail.com>
2022-05-17 19:57:03 -0700

    moved routes out to separate file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from 'react-auth-kit';
+import RoutesComponent from './components/Routes/Routes';
 import Header from './components/Header';
-import Login from './components/Routes/Login';
-import Home from './components/Homepage/Home';
-import Register from './components/Routes/Register';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MessageForm from './components/Routes/MessageForm';
 
 const App = () => {
 	return (
 		<>
-			<BrowserRouter>
-				<Header></Header>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/createpost" element={<MessageForm />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
-			</BrowserRouter>
+			<AuthProvider authType={'localstorage'} authName={'_auth'}>
+				<RoutesComponent />
+			</AuthProvider>
 		</>
 	);
 };
