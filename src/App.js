a26a73be9826b7e316000c35d6d81cb16a56cc19
ericchi00 <ericchi00@gmail.com>
2022-05-18 13:18:28 -0700

    removed header import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AuthProvider } from 'react-auth-kit';
 import RoutesComponent from './components/Routes/Routes';
-import Header from './components/Header';
 
 const App = () => {
 	return (
