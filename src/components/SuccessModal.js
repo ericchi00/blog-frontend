5e74e968ef28b1e9c0a61d77f98ec98f0ff51fce
ericchi00 <ericchi00@gmail.com>
2022-05-18 17:51:18 -0700

    changed href to router dom link

--- src/components/SuccessModal.js
+++ src/components/SuccessModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const SuccessModal = (props) => {
 	return (
@@ -12,11 +13,11 @@ const SuccessModal = (props) => {
 		>
 			<Modal.Body>
 				<h4>Register Sucess!</h4>
-				Page will redirect in a couple seconds. Click home if it does not.
+				Page will redirect in a couple seconds. Click Login if it does not.
 			</Modal.Body>
 			<Modal.Footer>
-				<Button href="/" variant="success">
-					Home
+				<Button variant="secondary" as={Link} to="/login">
+					Login
 				</Button>
 				<Button onClick={props.onHide}>Close</Button>
 			</Modal.Footer>
