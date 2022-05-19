00c465618832b970554c9f0e858c292e0fb39d8c
ericchi00 <ericchi00@gmail.com>
2022-05-19 12:04:03 -0700

    moved message card out to diff file

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import { format } from 'date-fns';
-
-const createMarkup = (html) => {
-	return { __html: html };
-};
+import MessageCard from './MessageCard';
 
 const Home = () => {
 	document.title = 'Message Board';
@@ -26,36 +21,11 @@ const Home = () => {
 	};
 
 	return (
-		<>
-			<Container fluid className="p-5 d-flex flex-wrap">
-				{messages.map((message, i) => {
-					return (
-						<Card key={message._id} style={{ width: '350px', margin: '1rem' }}>
-							<Card.Body>
-								<Card.Title>
-									<Card.Link href={`/messages/${message._id}`}>
-										{message.title}
-									</Card.Link>
-								</Card.Title>
-								<Card.Subtitle className="mb-2 text-muted">
-									<span>Published by: {message.username.username}</span>
-									<p> {format(new Date(message.date), 'Pp')}</p>
-								</Card.Subtitle>
-								<Card.Text
-									dangerouslySetInnerHTML={createMarkup(message.text)}
-								></Card.Text>
-								<Card.Link
-									href={`/messages/${message._id}`}
-									className="position-absolute bottom-0 end-0 p-2"
-								>
-									Comments
-								</Card.Link>
-							</Card.Body>
-						</Card>
-					);
-				})}
-			</Container>
-		</>
+		<Container fluid className="p-5 d-flex flex-wrap">
+			{messages.map((message, i) => {
+				return <MessageCard message={message} key={message._id} />;
+			})}
+		</Container>
 	);
 };
 
