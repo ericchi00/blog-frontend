ebee291afc7fffeafb4a7d587c072326c44a5a53
ericchi00 <ericchi00@gmail.com>
2022-05-23 12:43:30 -0700

    added in a container + footer

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -9,26 +9,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ErrorPage from './ErrorPage';
 import BlogPost from '../BlogPost/BlogPost';
 import BlogPostForm from './BlogPostForm';
+import Footer from '../Footer';
+import Container from 'react-bootstrap/Container';
 
 const RoutesComponent = () => {
 	return (
 		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route
-					path="/createpost"
-					element={
-						<RequireAuth loginPath={'/login'}>
-							<BlogPostForm />
-						</RequireAuth>
-					}
-				/>
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/blogposts/:id" element={<BlogPost />} />
-				<Route path="*" element={<ErrorPage />} />
-			</Routes>
+			<Container fluid style={{ padding: '0px', minHeight: '100vh' }}>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/createpost"
+						element={
+							<RequireAuth loginPath={'/login'}>
+								<BlogPostForm />
+							</RequireAuth>
+						}
+					/>
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/blogposts/:id" element={<BlogPost />} />
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</Container>
+			<Footer />
 		</BrowserRouter>
 	);
 };
