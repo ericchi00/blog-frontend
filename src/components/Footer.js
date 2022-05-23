f4e0b5dbef309fe5bb01e48e3f6a00e23911fb7f
ericchi00 <ericchi00@gmail.com>
2022-05-23 13:57:17 -0700

    fixed link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
 		<Navbar bg="dark" variant="dark" style={{ height: 'auto' }}>
 			<Container fluid className="justify-content-center">
 				<Navbar.Text>Built by Eric Chi.</Navbar.Text>
-				<Navbar.Link
-					as={Link}
-					to="https://api-only-backend-blog-react.herokuapp.com/"
-				>
+				<Navbar.Link href="https://api-only-backend-blog-react.herokuapp.com/">
 					Backend API
 				</Navbar.Link>
 			</Container>
